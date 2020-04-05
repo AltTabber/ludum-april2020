cea516d9a185e20a3f9c55356c6c936d94b0c47b
AltTabber <mikeice414@gmail.com>
2020-04-05 21:21:05 +0300

    микро коммит, что бы перекинуть код между компами - добавил координаты для юнитов

--- core/src/ru/alttabber/ludum/units/Player.java
+++ core/src/ru/alttabber/ludum/units/Player.java
@@ -1,4 +1,12 @@
 package ru.alttabber.ludum.units;
 
-public class Player {
+public class Player extends Unit {
+
+    public Player() {
+        super();
+        this.height = 100;
+        this.width = 100;
+    }
+
+    
 }
--- core/src/ru/alttabber/ludum/units/Unit.java
+++ core/src/ru/alttabber/ludum/units/Unit.java
@@ -2,6 +2,11 @@ package ru.alttabber.ludum.units;
 
 public class Unit {
 
+    protected int X;
+    protected int Y;
+    protected int height;
+    protected int width;
+
 
 
 }
