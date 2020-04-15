516913f8b24919accfda1ba332416921db47e621
AltTabber <mikeice414@gmail.com>
2020-04-16 00:41:43 +0300

    Изменил размер экрана

--- desktop/src/ru/alttabber/ludum/desktop/DesktopLauncher.java
+++ desktop/src/ru/alttabber/ludum/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Drop";
-		config.width = 1366;
-		config.height = 678;
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new LudumRogue(), config);
 	}
 }
