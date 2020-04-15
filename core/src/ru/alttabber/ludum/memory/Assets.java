ef5cac9d5eac188afc9ddb53b68384d4a604b5d4
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-04-16 02:49:52 +0300

    правка

--- core/src/ru/alttabber/ludum/memory/Assets.java
+++ core/src/ru/alttabber/ludum/memory/Assets.java
@@ -8,7 +8,7 @@ public class Assets {
     public static final String sideFrontTexture = "34front.png";
     public static final String sideBehindTexture = "34behind.png";
 
-    public static final String footStep = "sound/footStep";
+    public static final String footStep = "sound/footStep.wav";
     public static final String swordTexture = "sword.png";
 
 }
