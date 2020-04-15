86d759c0643f968d00954c409646354a6e54291a
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-04-16 00:58:40 +0300

    Добавил класс Window, хранящий параметры экрана. Добавил отрисовку managerBar.

---
+++ core/src/ru/alttabber/ludum/window/Window.java
@@ -0,0 +1,11 @@
+package ru.alttabber.ludum.window;
+
+public class Window {
+    private static int width = 1280;
+    private static int height = 720;
+    private  static String title = "SomeShit";
+
+    public static int getWidth(){ return width;}
+    public static int getHeight(){ return height;}
+    public static String getTitle(){ return title;}
+}
