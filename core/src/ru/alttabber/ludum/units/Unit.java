f91430aafaa7377b589d47e1e7b6360acfc356a9
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-04-16 01:49:06 +0300

    Менеджмент хитов и урона

--- core/src/ru/alttabber/ludum/units/Unit.java
+++ core/src/ru/alttabber/ludum/units/Unit.java
@@ -5,9 +5,41 @@ import ru.alttabber.ludum.abstracts.GameObject;
 public abstract class Unit extends GameObject {
 
     protected int hits;
+    protected int myDamage;
+    protected int damage;
 
-    public void setHits(int hits){
+    Unit(){
+        hits = 100;
+        damage = myDamage = 0;
+    }
+
+    public void setHits(int hits)
+    {
         this.hits = hits;
     }
     public int getHits(){return hits;}
+
+    public void takeDamage(int damage){
+        hits =- damage;
+        if(hits < 0){
+            hits = 0;
+        }
+    }
+
+    public void setDamage(int weaponDamage)
+    {
+        damage = myDamage + weaponDamage;
+    }
+    public int giveDamage(){
+        return damage;
+    }
+
+    boolean checkYourSelf(){
+        if(hits == 0)
+        {
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
