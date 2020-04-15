61746eca336512571f30afc74f52e1cca530de9a
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-04-16 02:47:43 +0300

    правка

--- core/src/ru/alttabber/ludum/LudumRogue.java
+++ core/src/ru/alttabber/ludum/LudumRogue.java
@@ -2,6 +2,7 @@ package ru.alttabber.ludum;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -43,6 +44,7 @@ public class LudumRogue extends ApplicationAdapter {
 
 		item = new SwordItem();
 		item.init(batch);
+
 	}
 
 	@Override
