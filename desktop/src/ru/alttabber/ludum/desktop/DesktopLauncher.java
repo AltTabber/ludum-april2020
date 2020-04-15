732d27a243a560af9aa4db7904c05bc38e6224c3
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-04-16 01:00:42 +0300

    Класс Window в Desktop

--- desktop/src/ru/alttabber/ludum/desktop/DesktopLauncher.java
+++ desktop/src/ru/alttabber/ludum/desktop/DesktopLauncher.java
@@ -3,13 +3,14 @@ package ru.alttabber.ludum.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.alttabber.ludum.LudumRogue;
+import ru.alttabber.ludum.window.Window;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
-		config.width = 1280;
-		config.height = 720;
+		config.title = Window.getTitle();
+		config.width = Window.getWidth();
+		config.height = Window.getHeight();
 		new LwjglApplication(new LudumRogue(), config);
 	}
 }
