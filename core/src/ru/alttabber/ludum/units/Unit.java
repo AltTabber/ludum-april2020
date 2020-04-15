f71552538668e473e5e57b11af4a7960fe21d67a
Курганников Андрей Алексеевич <kurgannikov.andrey@bk.ru>
2020-04-16 01:31:54 +0300

    правка

--- core/src/ru/alttabber/ludum/units/Unit.java
+++ core/src/ru/alttabber/ludum/units/Unit.java
@@ -6,4 +6,8 @@ public abstract class Unit extends GameObject {
 
     protected int hits;
 
+    public void setHits(int hits){
+        this.hits = hits;
+    }
+    public int getHits(){return hits;}
 }
