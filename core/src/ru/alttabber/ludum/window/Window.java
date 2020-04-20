b2f2952a47566b2728e3acb5f8a07b4743f90a99
AltTabber <mikeice414@gmail.com>
2020-04-21 02:35:53 +0300

    Изменил карту, изменил название игры

--- core/src/ru/alttabber/ludum/window/Window.java
+++ core/src/ru/alttabber/ludum/window/Window.java
@@ -3,7 +3,7 @@ package ru.alttabber.ludum.window;
 public class Window {
     private static int width = 1280;
     private static int height = 720;
-    private  static String title = "SomeShit";
+    private  static String title = "Keep the light alive: LudumDare-April2020";
 
     public static int getWidth(){ return width;}
     public static int getHeight(){ return height;}
